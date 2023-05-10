66a48aa25aeef8cbc3666d0ab3a77baabb99f833
robertkbk <33332309+robertkbk@users.noreply.github.com>
2023-05-10 22:33:38 +0200

    Refactored process.py

--- process.py
+++ process.py
@@ -1,45 +1,72 @@
-import os
-
+import sys
+from pathlib import Path
 import cv2
+import tap
+from tqdm import tqdm
 
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-x_start = 860
-x_end = 1207
-y_start = 85
-y_end = 730
 
-base_dir = "../data/audio(wideo)/non_animal/"
+class Arguments(tap.Tap):
+    dataset: Path = Path("../dane/audio(wideo)/non_animal/")
 
-dirs = [
+
+FOURCC = cv2.VideoWriter_fourcc(*"mp4v")
+
+XS = slice(860, 1207)
+YS = slice(85, 730)
+
+DIRS = [
     "A_non-contrast_1/A_non-contrast_1",
     "C_non-contrast_1/C_non-contrast_1",
     "D_non-contrast_1/D_non-contrast_1",
     "F_non-contrast_1/F_non-contrast_1",
 ]
 
-for data_dir in dirs:
-    dir = os.path.join(base_dir, data_dir)
-    for file_name in os.listdir(dir):
-        if file_name[-3:].lower() == "mp4":
-            print("found mp4 : ", file_name)
-            mp4_path = os.path.join(dir, file_name)
-            processed_dir = dir + "_processedd"
-            if not os.path.exists(processed_dir):
-                os.mkdir(processed_dir)
-            processed_mp4_path = os.path.join(processed_dir, file_name)
 
-            cap = cv2.VideoCapture(mp4_path)
-            # do not change last parameter
-            out = cv2.VideoWriter(processed_mp4_path, fourcc, 20, (347, 645))
+def frames(capture):
+    while capture.isOpened():
+        ret, frame = capture.read()
 
-            while cap.isOpened():
+        if not ret:
+            return
 
-                ret, frame = cap.read()
-                if not ret:
-                    print("finished processing file", file_name)
-                    break
-                therapy_frame = frame[y_start:y_end, x_start:x_end, :]
-                out.write(therapy_frame)
+        yield frame
 
-            out.release()
-            cap.release()
+
+def process_file(dir: Path, file: Path):
+    if not (file.is_file() and file.suffix.lower() == ".mp4"):
+        return
+
+    processed_dir = Path(str(dir) + "_processed")
+    processed_dir.mkdir(exist_ok=True)
+    processed = processed_dir / file.name
+
+    cap = cv2.VideoCapture(str(file))
+    # do not change last parameter
+    out = cv2.VideoWriter(
+        str(processed), FOURCC, 20, (XS.stop - XS.start, YS.stop - YS.start)
+    )
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for frame in tqdm(frames(cap), total=frame_count, desc=file.name):
+        therapy_frame = frame[YS, XS, :]
+        out.write(therapy_frame)
+
+    out.release()
+    cap.release()
+
+
+def main(dataset: Path):
+    for dirname in DIRS:
+        dir = dataset / dirname
+
+        if not dir.exists():
+            print(f"error: directory {dir} does not exist", file=sys.stderr)
+            continue
+
+        for file in dir.iterdir():
+            process_file(dir, file)
+
+
+if __name__ == "__main__":
+    args = Arguments().parse_args()
+    main(args.dataset)
