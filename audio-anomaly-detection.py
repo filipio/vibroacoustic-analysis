fd49762802524beab9a6d379a07dad936cc55235
Filip Juza <filip.juza.2000@gmail.com>
2023-05-13 19:23:46 +0200

    add saving of anomaly detection results

--- audio-anomaly-detection.py
+++ audio-anomaly-detection.py
@@ -1,3 +1,5 @@
+import os
+
 import librosa
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,16 +9,11 @@ from streamad.model import (
     MadDetector,
     OCSVMDetector,
     RrcfDetector,
-    SArimaDetector,
     SpotDetector,
     SRDetector,
-    ZScoreDetector,
-    xStreamDetector,
 )
-from streamad.process import TDigestCalibrator, WeightEnsemble, ZScoreCalibrator
+from streamad.process import ZScoreCalibrator
 from streamad.util import CustomDS, StreamGenerator, plot
-
-# ZScore fucked up
 
 SIGNAL_SAMPLING = 44_500
 CLEAN_AUDIO_CUTOFF_INDEX = int(SIGNAL_SAMPLING / 2)
@@ -24,6 +21,8 @@ AUDIO_PEAK_INDEX_BOUNDARY = 300
 AUDIO_PATH = (
     "../data/audio(wideo)/non_animal/D_non-contrast_1/D_non-contrast_1/REC70.WAV"
 )
+RESULTS_PATH = "./anomaly_detection_results"
+SAVE_FILE_FORMAT = ".svg"
 
 
 def load_audio_data(path):
@@ -37,36 +36,66 @@ def plot_audio(signal):
     plt.show()
 
 
-audio = load_audio_data(AUDIO_PATH)
+def save_scores(path, dataset, scores):
+    data, label, date, _ = dataset.data, dataset.label, dataset.date, dataset.features
+    fig = plot(
+        data=data, scores=scores, date=date, features=["original_audio"], label=label
+    )
 
-audio_clean = audio[CLEAN_AUDIO_CUTOFF_INDEX:-CLEAN_AUDIO_CUTOFF_INDEX]
-peak_index = np.argmax(audio_clean)
-original_peak_index = CLEAN_AUDIO_CUTOFF_INDEX + peak_index
-print(original_peak_index)
-peak_audio = audio[
-    original_peak_index
-    - AUDIO_PEAK_INDEX_BOUNDARY : original_peak_index
-    + AUDIO_PEAK_INDEX_BOUNDARY
-]
+    fig.write_image(path)
 
 
-ds = CustomDS(peak_audio)
-stream = StreamGenerator(ds.data)
-model = SpotDetector()
-calibrator = ZScoreCalibrator()
+def main():
+    audio = load_audio_data(AUDIO_PATH)
+    if not os.path.exists(RESULTS_PATH):
+        os.mkdir(RESULTS_PATH)
 
-scores = []
-iter = 0
+    audio_clean = audio[CLEAN_AUDIO_CUTOFF_INDEX:-CLEAN_AUDIO_CUTOFF_INDEX]
+    peak_index = np.argmax(audio_clean)
+    original_peak_index = CLEAN_AUDIO_CUTOFF_INDEX + peak_index
+    peak_audio = audio[
+        original_peak_index
+        - AUDIO_PEAK_INDEX_BOUNDARY : original_peak_index
+        + AUDIO_PEAK_INDEX_BOUNDARY
+    ]
 
-for x in stream.iter_item():
-    score = model.fit_score(x)
-    normalized_score = score
-    # uncomment line below to use normalization calibrator
-    # normalized_score = calibrator.normalize(score)
-    scores.append(normalized_score)
-    print(f"added score {iter + 1}")
-    iter += 1
+    models = [
+        SpotDetector(),
+        KNNDetector(),
+        LodaDetector(),
+        MadDetector(),
+        OCSVMDetector(),
+        RrcfDetector(),
+        SRDetector(),
+    ]
 
-data, label, date, features = ds.data, ds.label, ds.date, ds.features
-fig = plot(data=data, scores=scores, date=date, features=features, label=label)
-fig.show()
+    for model in models:
+        model_name = model.__class__.__name__
+        ds = CustomDS(peak_audio)
+        stream = StreamGenerator(ds.data)
+        print(model.__class__.__name__)
+        calibrator = ZScoreCalibrator()
+        scores = []
+        scores_calibrated = []
+
+        for x in stream.iter_item():
+            score = model.fit_score(x)
+            score_calibrated = calibrator.normalize(score)
+            scores.append(score)
+            scores_calibrated.append(score_calibrated)
+
+        print(f"finished processing for model {model_name}")
+
+        model_results_path = os.path.join(RESULTS_PATH, model_name + SAVE_FILE_FORMAT)
+        calibrated_model_results_path = os.path.join(
+            RESULTS_PATH, model_name + "_calibrated" + SAVE_FILE_FORMAT
+        )
+
+        save_scores(path=model_results_path, dataset=ds, scores=scores)
+        save_scores(
+            path=calibrated_model_results_path, dataset=ds, scores=scores_calibrated
+        )
+
+
+if __name__ == "__main__":
+    main()
